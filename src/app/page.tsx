a408c6568d339b0facbea6c2ea64e94e999c87c8
sahiltolani30 <sahiltolani30@users.noreply.github.com>
2026-09-02 04:08:12 +0530

    chore: isolate concept 7 on test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Problem from "@/components/Problem";
 import ProblemV2BrokenFunnel from "@/components/ProblemV2BrokenFunnel";
 import Approach from "@/components/Approach";
 import Services from "@/components/Services";
-import ConceptJ_FilterableGrid from "@/components/featured-work/ConceptJ_FilterableGrid";
+import Concept5Accordion from "@/components/Concept5Accordion";
 import HowItWorks from "@/components/HowItWorks";
 import Footer from "@/components/Footer";
 import SuccessV2C7BentoKPI from "@/components/SuccessV2C7BentoKPI";
@@ -30,8 +30,9 @@ export default async function Home() {
       <ProblemV2BrokenFunnel />
       <Services />
 
-      {/* TEMPORARY CONCEPT 7 TEST IN PRODUCTION */}
-      <ConceptJ_FilterableGrid featuredWorkVideos={featuredWorkVideos} />
+      {/* 3 NEW CONCEPTS FOR REVIEW */}
+      <Concept5Accordion featuredWorkVideos={featuredWorkVideos} />
+
 
       <Approach />
       {/* 10 NEW "OUR APPROACH" V2 CONCEPTS FOR REVIEW */}
